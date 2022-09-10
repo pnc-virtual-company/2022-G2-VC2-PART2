81be1c33856e7bf5b7fa3a4b5b4ea222667c487d
phandyphorn <phornphandy20@gmail.com>
2022-09-10 13:15:50 +0700

    #27 coordinator Navbar

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import CoorTeacherList from '../views/CoordinatorView/CoorTeacherList.vue'
+import CoorStudentList from '../views/CoordinatorView/CoorStudentList.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'teacherList',
+    component: CoorTeacherList
   },
+
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/studentList',
+    name: 'studentList',
+    component: CoorStudentList
+  },
+  
 ]
 
 const router = createRouter({
