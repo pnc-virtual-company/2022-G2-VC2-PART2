dcaf40cd3861953de28e5c69ab6284d2a297a386
Sopha-Rath <sopha.rath@student.passerellesnumeriques.org>
2022-09-15 13:01:59 +0700

    #55

--- front/src/components/coordinator/teacher/CardDetail.vue
+++ front/src/components/coordinator/teacher/CardDetail.vue
@@ -9,7 +9,7 @@
           <div class="modal-container bg-blue-200">
               <div class="flex space-x-[50px]">
                   <div class="ml-[50px]">
-                      <img :src="user_info.users.profile" width="126" class="mt-5 rounded-full">
+                      <img :src="user_info.users.profile" width="126" class="mt-5 rounded-full flex justify-center m-auto">
                       <p class="text-center text-xl font-bold mb-5">{{user_info.users.first_name}} {{user_info.users.last_name}}</p>
                   </div>
                   <div class="mt-[18px] leading-loose">
