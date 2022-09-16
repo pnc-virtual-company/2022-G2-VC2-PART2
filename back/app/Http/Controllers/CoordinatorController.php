4f9abb4fe460fee427375f7ee42ff77cd5ddbb71
sophimkep <sophimkep95@gmail.com>
2022-09-16 16:09:29 +0700

    #39 pull

--- back/app/Http/Controllers/CoordinatorController.php
+++ back/app/Http/Controllers/CoordinatorController.php
@@ -8,18 +8,5 @@ use Illuminate\Http\Request;
 
 class CoordinatorController extends Controller
 {
-    public function create_coordinator(Request $request)
-    {
-        //
-        $coordinator = new Coordinator();
-        $user = new User();
-        $user->first_name = $request->first_name;
-        $user->last_name = $request->last_name;
-        $user->email = $request->email;
-        $user->gender = $request->gender;
-        $user->role = 1;
-        $user->save();
-        $user->coordinators()->save($coordinator);
-        return "Create successfully";
-    }
+   
 }
--- back/database/migrations/2014_10_12_000000_create_users_table.php
+++ back/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,9 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 return new class extends Migration
 {
     /**
@@ -24,6 +26,7 @@ return new class extends Migration
         });
 
     }
+
     /**
      * Reverse the migrations.
      *
