9cf86aca076ac29683edd14fd84186a2b58e3773
sarathorn11 <raths8546@gmail.com>
2022-09-18 09:11:30 +0700

    #39 bug

--- front/src/components/teachers/table/StudentFollowList.vue
+++ front/src/components/teachers/table/StudentFollowList.vue
@@ -18,6 +18,7 @@
             Choose
           </option>
           <option value="">Show All</option>
+          <option value="2022">2022</option>
           <option value="2023">2023</option>
           <option value="2024">2024</option>
           <option value="2025">2025</option>
