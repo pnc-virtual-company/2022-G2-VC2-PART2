6dc3eb06d4a06e48dbdb3b55c62e593f22051556
sarathorn11 <raths8546@gmail.com>
2022-09-28 14:24:46 +0700

    #39

--- front/src/components/students/studentView/TutorCommentView.vue
+++ front/src/components/students/studentView/TutorCommentView.vue
@@ -27,32 +27,17 @@
 </template>
 
 <script>
-import axiosClient from "../../../axios-http";
+
 export default {
   data(){
     return{
-      tutor:'',
-      comments: []
-
+      
     }
 
   },
    methods:{
-    get_tutor(){
-      let user_id = this.$cookies.get('user_id');
-       axiosClient.get("students/get/"+user_id).then((res) => {
-         this.tutor=res.data;
-        console.log(this.tutor);
-      });
-    },
+   
 
-    get_comment_tutor(){
-      let user_id = this.$cookies.get('user_id');
-      axiosClient.get("comments/get" + user_id).then((res) => {
-        this.comments = res.data;
-        
-      })
-    }
 
   },
   mounted() {
